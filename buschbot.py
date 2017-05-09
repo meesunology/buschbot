13698d8281b181039929e0acf8d42462f9801b08
Meesun Park <meesun.park@rutgers.edu>
2017-05-09 11:40:55 -0400

    Committing Changes

---
+++ buschbot.py
@@ -0,0 +1,13 @@
+import os
+import time
+from slackclient import SlackClient
+
+#buschbot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+
+#constants 
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+
+#instantiate Slack & Twilio clients
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
