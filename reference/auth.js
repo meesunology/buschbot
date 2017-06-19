4a3d56d3c1a133db889c88feea5efa443828c3e0
Meesun Park <meesun.park@rutgers.edu>
2017-06-19 15:01:18 -0400

    update

--- reference/auth.js
+++ reference/auth.js
@@ -1,5 +1,5 @@
 // After adding your tokens, be sure to run 'git update-index --assume-unchanged auth.js'
 // If not done, every time you pull from the repo, the tokens will be overwritten.
-var slackbot_token = 'xoxb-180766532327-vyVTbnE08qhThFJzE0snOcRu'; // SLACKBOT TOKEN HERE
+var slackbot_token = ''; // SLACKBOT TOKEN HERE
 
 module.exports.slackbot = slackbot_token;
